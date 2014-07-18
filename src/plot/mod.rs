0feae0dc585cee7996b658720edda009a534747b
Jorge Aparicio <japaric@linux.com>
2014-07-18 17:16:27 -0500

    Summary plot: Fix logic to trigger log scale

--- src/plot/mod.rs
+++ src/plot/mod.rs
@@ -180,7 +180,7 @@ pub fn summarize(dir: &Path) {
 
             fs::mkdirp(&dir.join(format!("summary/{}", sample)));
             Figure::new().
-                set_logscale((points.last().unwrap() / points[0] > 50.0, false)).
+                set_logscale((points[0] / *points.last().unwrap() > 50.0, false)).
                 set_output_file(dir.join(format!("summary/{}/{}s.png", sample, statistic))).
                 set_size(PNG_SIZE).
                 set_title(format!("{}", statistic)).
