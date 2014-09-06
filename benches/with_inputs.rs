e9b53221fddbd1cd6e874ad20e0e8c3edd52a05f
Jorge Aparicio <japaric@linux.com>
2014-09-06 18:05:22 -0500

    Fix generation of summary plots, add a test for it

--- benches/with_inputs.rs
+++ benches/with_inputs.rs
@@ -7,4 +7,8 @@ fn from_elem() {
     Criterion::default().bench_with_inputs("from_elem", |b, &size| {
         b.iter(|| Vec::from_elem(size, 0u8));
     }, [1024, 2048, 4096]);
+
+    // Check that the summary plots have been generated
+    let summary_dir = Path::new(".criterion/from_elem/summary/new");
+    assert!(summary_dir.join("means.svg").exists() && summary_dir.join("medians.svg").exists())
 }
