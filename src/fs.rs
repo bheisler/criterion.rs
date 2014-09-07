e6140c7c9b694f48830058f4e520a9df51692012
Jorge Aparicio <japaric@linux.com>
2014-09-06 19:12:58 -0500

    Pretty JSON encoding

--- src/fs.rs
+++ src/fs.rs
@@ -1,5 +1,7 @@
+use serialize::json::PrettyEncoder;
 use serialize::{Decodable, Encodable, json};
-use std::io::{File, IoError, UserRWX, fs};
+use std::io::{File, IoError, MemWriter, UserRWX, fs};
+use std::mem;
 
 // TODO Proper error handling
 pub fn load<A: Decodable<json::Decoder, json::DecoderError>>(path: &Path) -> A {
@@ -36,7 +38,13 @@ pub fn rmrf(path: &Path) {
 }
 
 // TODO Proper error handling
-pub fn save<'a, D: Encodable<json::Encoder<'a>, IoError>>(data: &D, path: &Path) {
-    File::create(path).write_str(json::encode(data).as_slice()).ok().
+pub fn save<'a, D: Encodable<json::PrettyEncoder<'a>, IoError>>(data: &D, path: &Path) {
+    let mut writer = MemWriter::new();
+    {
+        let ref mut encoder = PrettyEncoder::new(&mut writer);
+        // FIXME (rust-lang/rust#14302) Remove transmute
+        data.encode(unsafe { mem::transmute(encoder) }).unwrap();
+    }
+    File::create(path).write(writer.get_ref()).ok().
         expect("Couldn't save data")
 }
