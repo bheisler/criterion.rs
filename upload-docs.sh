6e0f900f574a2edffd88dcca370b6f92daadc27f
Jorge Aparicio <japaricious@gmail.com>
2014-12-13 21:34:46 -0500

    chore(travis): fix doc upload remote

--- upload-docs.sh
+++ upload-docs.sh
@@ -3,7 +3,7 @@ cd target/doc
 git init
 git config user.name "Jorge Aparicio"
 git config user.email "japaricious@gmail.com"
-git remote add origin "https://$GH_TOKEN@github.com/japaric/simplot.rs"
+git remote add origin "https://$GH_TOKEN@github.com/japaric/stats.rs"
 
 git checkout -b gh-pages
 git add -A
