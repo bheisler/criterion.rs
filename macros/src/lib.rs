eb40b0ecc02f65633ff1f2686b70223a15d10e40
Thomas Jespersen <laumann.thomas@gmail.com>
2015-06-06 01:29:58 +0200

    Use "if let" instead of match

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -51,48 +51,45 @@ fn expand_meta_criterion(
     item: Annotatable,
 ) -> Annotatable {
     if let Annotatable::Item(item) = item {
-        match item.node {
-            ItemFn(..) => {
-                // Copy original function without attributes
-                let routine = P(Item { attrs: Vec::new(), .. (*item).clone() });
+        if let ItemFn(..) = item.node {
+            // Copy original function without attributes
+            let routine = P(Item { attrs: Vec::new(), .. (*item).clone() });
 
-                // `::criterion::Criterion::default()`
-                let crate_ident = token::str_to_ident("criterion");
-                let struct_ident = token::str_to_ident("Criterion");
-                let method_ident = token::str_to_ident("default");
-                let criterion_path = vec!(crate_ident, struct_ident, method_ident);
-                let default_criterion = cx.expr_call_global(span, criterion_path, vec!());
+            // `::criterion::Criterion::default()`
+            let crate_ident = token::str_to_ident("criterion");
+            let struct_ident = token::str_to_ident("Criterion");
+            let method_ident = token::str_to_ident("default");
+            let criterion_path = vec!(crate_ident, struct_ident, method_ident);
+            let default_criterion = cx.expr_call_global(span, criterion_path, vec!());
 
-                // `.bench("routine", routine);`
-                let routine_str = cx.expr_str(span, token::get_ident(routine.ident));
-                let routine_ident = cx.expr_ident(span, routine.ident);
-                let bench_ident = token::str_to_ident("bench");
-                let bench_call = cx.expr_method_call(span, default_criterion, bench_ident,
-                                                     vec!(routine_str, routine_ident));
-                let bench_call = cx.stmt_expr(bench_call);
+            // `.bench("routine", routine);`
+            let routine_str = cx.expr_str(span, token::get_ident(routine.ident));
+            let routine_ident = cx.expr_ident(span, routine.ident);
+            let bench_ident = token::str_to_ident("bench");
+            let bench_call = cx.expr_method_call(span, default_criterion, bench_ident,
+                                                 vec!(routine_str, routine_ident));
+            let bench_call = cx.stmt_expr(bench_call);
 
-                // Wrap original function + bench call in a test function
-                let fn_decl = P(codemap::respan(span, DeclItem(routine)));
-                let inner_fn = P(codemap::respan(span, StmtDecl(fn_decl, DUMMY_NODE_ID)));
-                let body = cx.block(span, vec!(inner_fn, bench_call), None);
-                let nil = P(ast::Ty {
-                    id: ast::DUMMY_NODE_ID,
-                    node: ast::TyTup(vec![]),
-                    span: codemap::DUMMY_SP,
-                });
-                let test = cx.item_fn(span, item.ident, Vec::new(), nil, body);
+            // Wrap original function + bench call in a test function
+            let fn_decl = P(codemap::respan(span, DeclItem(routine)));
+            let inner_fn = P(codemap::respan(span, StmtDecl(fn_decl, DUMMY_NODE_ID)));
+            let body = cx.block(span, vec!(inner_fn, bench_call), None);
+            let nil = P(ast::Ty {
+                id: ast::DUMMY_NODE_ID,
+                node: ast::TyTup(vec![]),
+                span: codemap::DUMMY_SP,
+            });
+            let test = cx.item_fn(span, item.ident, Vec::new(), nil, body);
 
-                // Add the `#[test]` attribute to existing attributes
-                let mut attrs = item.attrs.clone();
-                attrs.
-                    push(cx.attribute(span, cx.meta_word(span, token::intern_and_get_ident("test"))));
+            // Add the `#[test]` attribute to existing attributes
+            let mut attrs = item.attrs.clone();
+            attrs.
+                push(cx.attribute(span, cx.meta_word(span, token::intern_and_get_ident("test"))));
 
-                Annotatable::Item(P(Item { attrs: attrs, .. (*test).clone() }))
-            },
-            _ => {
-                cx.span_err(span, "#[criterion] only supported on functions");
-                Annotatable::Item(item)
-            },
+            Annotatable::Item(P(Item { attrs: attrs, .. (*test).clone() }))
+        } else {
+            cx.span_err(span, "#[criterion] only supported on functions");
+            Annotatable::Item(item)
         }
     } else {
         cx.span_err(span, "#[criterion] only supported on functions");
