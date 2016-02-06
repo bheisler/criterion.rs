3cc52da71915bc5cce021e873af7cad5f5a3285e
Alex-Diez <alexvic.amatory@gmail.com>
2016-02-06 13:41:51 +0200

    compile with nightly rustc

--- benches/no-plots.rs
+++ benches/no-plots.rs
@@ -1,17 +1,15 @@
-#![feature(fs_walk)]
-
 extern crate criterion;
-
-use std::fs;
-use std::path::Path;
+extern crate walkdir;
 
 use criterion::Criterion;
+
+use walkdir::WalkDir;
 
 #[test]
 fn no_plots() {
     Criterion::default().without_plots().bench("dummy", |b| b.iter(|| {}));
 
-    assert!(!fs::walk_dir(&Path::new(".criterion/dummy")).ok().unwrap().any(|entry| {
+    assert!(!WalkDir::new(".criterion/dummy").into_iter().any(|entry| {
         entry.unwrap().path().extension().and_then(|ext| ext.to_str()) == Some("svg")
     }))
 }
