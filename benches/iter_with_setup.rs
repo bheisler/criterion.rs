8338ecae43c08d1c42bc0ebf5921c4308409476a
Jorge Aparicio <japaricious@gmail.com>
2016-02-08 21:30:41 -0500

    add new timing loops

---
+++ benches/iter_with_setup.rs
@@ -0,0 +1,14 @@
+extern crate criterion;
+
+use std::mem;
+
+use criterion::Criterion;
+
+const SIZE: usize = 1024 * 1024;
+
+#[test]
+fn dealloc() {
+    Criterion::default()
+        .bench("dealloc", |b| b.iter_with_setup(|| (0..SIZE).map(|_| 0u8).collect::<Vec<_>>(),
+                                                |v| mem::drop(v)));
+}
