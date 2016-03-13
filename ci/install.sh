16dbc9c2d96b2397d3fc8b8fea806acb128de555
Benedikt Steinbusch <ben@rockshrub.de>
2016-03-13 21:27:13 +0100

    add clippy

--- ci/install.sh
+++ ci/install.sh
@@ -1,5 +1,12 @@
 set -ex
 
+oldpath=$(pwd)
+cd $HOME
+git clone https://github.com/arcnmx/cargo-clippy
+cd $HOME/cargo-clippy
+cargo build --release
+cd $oldpath
+
 if [ "$TRAVIS_OS_NAME" = "osx" ] && [ "$GNUPLOT" = "yes" ]; then
   brew install gnuplot
 fi
