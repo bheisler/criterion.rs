9c41ed59437668a1fd28b958e9018c36bfaf9e94
Brook Heisler <redattack34@gmail.com>
2017-09-10 16:33:07 -0600

    Switch to using the official cargo clippy for CI.

--- ci/install.sh
+++ ci/install.sh
@@ -1,10 +1,5 @@
 set -ex
 
 if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
-  oldpath=$(pwd)
-  cd $HOME
-  git clone https://github.com/arcnmx/cargo-clippy
-  cd $HOME/cargo-clippy
-  cargo build --release
-  cd $oldpath
+  cargo install clippy
 fi
