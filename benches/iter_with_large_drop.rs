0d9fb9248c74d9f635040f10ec92f0e67e9b2beb
Brook Heisler <redattack34@gmail.com>
2017-09-14 18:38:57 -0600

    Reduce warmup time in some benchmarks to avoid running out of memory on Travis.

--- benches/iter_with_large_drop.rs
+++ benches/iter_with_large_drop.rs
@@ -1,13 +1,15 @@
 extern crate criterion;
 
 use criterion::Criterion;
+use std::time::Duration;
 
 const SIZE: usize = 1024 * 1024;
 
 #[test]
 fn alloc() {
-    Criterion::default()
-        .bench_function("alloc", |b| {
+    let mut c = Criterion::default();
+    c.warm_up_time(Duration::new(1, 0));
+    c.bench_function("alloc", |b| {
             b.iter_with_large_drop(|| (0..SIZE).map(|_| 0u8).collect::<Vec<_>>())
         });
 }
