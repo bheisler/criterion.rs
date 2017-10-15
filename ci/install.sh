aa083badfdbd7e45ae16991feea7c34533bc0910
Brook Heisler <redattack34@gmail.com>
2017-10-14 20:02:46 -0600

    Attempt to enable caching.

--- ci/install.sh
+++ ci/install.sh
@@ -1,6 +1,26 @@
 set -ex
 
-cargo install clippy
+do_install() {
+    app=$1
+
+    # Attempt to install app
+    ret=0
+    cargo install $app || ret=$?
+
+    # If it failed (most likely already installed) then check versions and update if necessary.
+    if [ 101 = $ret ]; then
+        new_version=`cargo install $app 2>&1 | grep "Installing $app" | grep -o "[0-9.]*" || true`
+        installed_version=`cargo install --list | grep "$app v" | grep -o "[0-9.]*" || true`
+        if [[ "$new_version" != "$installed_version" ]]; then
+            cargo install $app --force
+        fi
+    fi
+}
+
+do_install clippy
+if [ "$TRAVIS_OS_NAME" = "linux"]; then
+    do_install mdbook
+fi
 
 if [ "$TRAVIS_OS_NAME" = "osx" ] && [ "$GNUPLOT" = "yes" ]; then
     brew update
