ec1e1bf4f3d2af70001550025c420b214d87c978
Brook Heisler <redattack34@gmail.com>
2017-12-19 18:45:59 -0600

    Fix flaky stats tests. Fixes #92.

--- stats/src/test.rs
+++ stats/src/test.rs
@@ -1,7 +1,7 @@
 use rand::{Rand, Rng, XorShiftRng, self};
 
 pub fn vec<T>(size: usize, start: usize) -> Option<Vec<T>> where T: Rand {
-    if size > start + 1 {
+    if size > start + 2 {
         let mut rng: XorShiftRng = rand::thread_rng().gen();
 
         Some((0..size).map(|_| rng.gen()).collect())
