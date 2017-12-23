f126105102c76102dd4fe497cea9f4d1dc3c3b0f
torkleyy <torkleyy@gmail.com>
2017-12-23 13:31:49 +0100

    Don't panic on errors

--- src/fs.rs
+++ src/fs.rs
@@ -5,63 +5,51 @@ use serde_json;
 use serde::Serialize;
 use serde::de::DeserializeOwned;
 
-// TODO Proper error handling
-pub fn load<A, P: ?Sized>(path: &P) -> A where
+use error::{AccessError, Result};
+
+pub fn load<A, P: ?Sized>(path: &P) -> Result<A> where
     A: DeserializeOwned,
     P: AsRef<Path>,
 {
-
-    let mut f = File::open(path).unwrap_or_else(|e| { panic!("{}", e) });
+    let mut f = File::open(path)
+        .map_err(|inner| AccessError { inner, path: path.as_ref().to_owned() })?;
     let mut string = String::new();
     let _ = f.read_to_string(&mut string);
-    let result: A = serde_json::from_str(string.as_str()).unwrap();
-    result
+    let result: A = serde_json::from_str(string.as_str())?;
+
+    Ok(result)
 }
 
-pub fn ls(dir: &Path) -> ReadDir {
-    match fs::read_dir(dir) {
-        Err(e) => panic!("`ls {}`: {}", dir.display(), e),
-        Ok(contents) => contents,
-    }
+pub fn ls(dir: &Path) -> Result<ReadDir> {
+    Ok(fs::read_dir(dir)?)
 }
 
-pub fn mkdirp<P>(path: &P) where
+pub fn mkdirp<P>(path: &P) -> Result<()> where
     P: AsRef<Path>,
 {
-    fn mkdirp_(path: &Path) {
-        if let Err(e) = fs::create_dir_all(path) {
-            panic!("`mkdir -p {}`: {}", path.display(), e)
-        }
-    }
-
-    mkdirp_(path.as_ref())
+    Ok(fs::create_dir_all(path.as_ref())?)
 }
 
-pub fn mv(from: &Path, to: &Path) {
-    if let Err(e) = fs::rename(from, to) {
-        panic!("`mv {} {}`: {}", from.display(), to.display(), e)
-    }
+pub fn mv(from: &Path, to: &Path) -> Result<()> {
+    Ok(fs::rename(from, to)?)
 }
 
-pub fn rmrf(path: &Path) {
-    if let Err(e) = fs::remove_dir_all(path) {
-        panic!("`rm -rf {}`: {}", path.display(), e)
-    }
+pub fn rmrf(path: &Path) -> Result<()> {
+    fs::remove_dir_all(path)?;
+
+    Ok(())
 }
 
-// TODO Proper error handling
-pub fn save<D, P>(data: &D, path: &P) where
+pub fn save<D, P>(data: &D, path: &P) -> Result<()> where
     D: Serialize,
     P: AsRef<Path>,
 {
-    fn save_<D>(data: &D, path: &Path) where
-        D: Serialize,
-    {
-        use std::io::Write;
+    use std::io::Write;
 
-        let buf = serde_json::to_string(&data).unwrap();
-        File::create(path).unwrap().write_all(buf.as_bytes()).expect("Couldn't save data")
-    }
+    let buf = serde_json::to_string(&data)?;
+    File::create(path)
+        .and_then(|mut f| f.write_all(buf.as_bytes()))
+        .map_err(|inner| AccessError { inner, path: path.as_ref().to_owned() })?;
 
-    save_(data, path.as_ref())
+    Ok(())
 }
