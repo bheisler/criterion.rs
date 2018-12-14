799a4b15a7b00bf1b395f855fa9bf2940f7b81b6
Brook Heisler <redattack34@gmail.com>
2018-12-13 19:13:07 -0600

    Add initial work on bencher compatibility layer.
    Very incomplete, but enough to at least run the example benchmark.

---
+++ bencher_compat/benches/bencher_example.rs
@@ -0,0 +1,24 @@
+#[macro_use]
+extern crate criterion;
+#[macro_use]
+extern crate criterion_bencher_compat;
+
+use criterion_bencher_compat::Bencher;
+
+fn a(bench: &mut Bencher) {
+    bench.iter(|| {
+        (0..1000).fold(0, |x, y| x + y)
+    })
+}
+
+fn b(bench: &mut Bencher) {
+    const N: usize = 1024;
+    bench.iter(|| {
+        vec![0u8; N]
+    });
+
+    bench.bytes = N as u64;
+}
+
+benchmark_group!(benches, a, b);
+benchmark_main!(benches);
---
+++ bencher_compat/src/lib.rs
@@ -0,0 +1,47 @@
+extern crate criterion;
+
+pub struct Bencher<'a> {
+    pub bytes: u64,
+    pub bencher: &'a mut ::criterion::Bencher,
+}
+impl<'a> Bencher<'a> {
+    pub fn iter<T, F>(&mut self, inner: F)
+        where F: FnMut() -> T
+    {
+        self.bencher.iter(inner);
+    }
+}
+
+#[macro_export]
+macro_rules! benchmark_group {
+    ($group_name:ident, $($function:path),+) => {
+        pub fn $group_name() {
+            use criterion::Criterion;
+            let mut criterion: Criterion = Criterion::default().configure_from_args();
+
+            $(
+                criterion.bench_function(stringify!($function), |b| {
+                    let mut wrapped = $crate::Bencher {
+                        bytes: 0,
+                        bencher: b,
+                    };
+
+                    $function(&mut wrapped);
+                });
+            )+
+        }
+    };
+    ($group_name:ident, $($function:path,)+) => {
+        benchmark_group!($group_name, $($function),+);
+    };
+}
+
+#[macro_export]
+macro_rules! benchmark_main {
+    ($($group_name:path),+) => {
+        criterion_main!($($group_name,)+);
+    };
+    ($($group_name:path,)+) => {
+        benchmark_main!($($group_name),+);
+    };
+}
