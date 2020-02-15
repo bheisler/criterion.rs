1237d275c38edead023b0ad8654c25f5255ec647
Tom Parker-Shemilt <palfrey@tevp.net>
2020-02-15 15:07:14 +0000

    Unlink python@2 so gnuplot install works

--- ci/install.sh
+++ ci/install.sh
@@ -10,6 +10,7 @@ if [ "$DOCS" = "yes" ]; then
 fi
 
 if [ "$TRAVIS_OS_NAME" = "osx" ] && [ "$GNUPLOT" = "yes" ]; then
+    brew unlink python@2 # because we're installing python3 and they both want to install stuff under /usr/local/Frameworks/Python.framework/
     brew install gnuplot
 fi
 
