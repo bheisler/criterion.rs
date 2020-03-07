00851159749a7415a9ceed569cef59e1fafbdc4c
Brook Heisler <redattack34@gmail.com>
2020-03-07 10:28:40 -0600

    Clean up the CI install script.

--- ci/install.sh
+++ ci/install.sh
@@ -1,19 +1,11 @@
 set -ex
 
-if [ "$CLIPPY" = "yes" ]; then
-    rustup component add clippy-preview
-fi
-
 if [ "$DOCS" = "yes" ]; then
-    cargo install mdbook --no-default-features --force
-    cargo install mdbook-linkcheck --force
+    cargo install mdbook --no-default-features
+    cargo install mdbook-linkcheck
 fi
 
 if [ "$TRAVIS_OS_NAME" = "osx" ] && [ "$GNUPLOT" = "yes" ]; then
     brew unlink python@2 # because we're installing python3 and they both want to install stuff under /usr/local/Frameworks/Python.framework/
     brew install gnuplot
-fi
-
-if [ "$RUSTFMT" = "yes" ]; then
-    rustup component add rustfmt-preview
 fi
