08bdb9ace9c1d237c0a5a79a1844dc0a894056b2
David Himmelstrup <lemmih@gmail.com>
2022-02-13 12:05:07 +0000

    Improved WASI support (#550)
    * Document WASM support with wasmer, wasmtime, nodejs, and browsers.

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,9 @@
     )
 )]
 
+#[cfg(all(feature = "rayon", target_arch = "wasm32"))]
+compile_error!("Rayon cannot be used when targeting wasi32. Try disabling default features.");
+
 #[cfg(test)]
 extern crate approx;
 
